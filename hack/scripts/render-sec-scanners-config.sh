cd35ccc59409130b956a9ce171886555400c2b1c
Friedrich Wilken <jens.wilken@sap.com>
2024-01-24 15:53:15 +0100

    render version

--- hack/scripts/render-sec-scanners-config.sh
+++ hack/scripts/render-sec-scanners-config.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+TAG=$1
 OUTPUT_FILE="sec-scanners-config.yaml"
 CURRENT_TIME=$(date +"%T")
 
 # Generating File.
 echo -e "generating to ${OUTPUT_FILE} \n"
 cat <<EOF | tee ${OUTPUT_FILE}
+${TAG}
 Hello
 ${CURRENT_TIME}
 EOF
